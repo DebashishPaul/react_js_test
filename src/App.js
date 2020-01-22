2848d2c91623181289621403d293ba0a0935d6a1
Debashish Paul <debashish333paul@gmail.com>
2020-01-22 23:28:43 +0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Paul from "./components/Paul";
 import Welcome from "./components/WelcomeClass";
 import Next3 from "./components/Next3";
+import Counter from "./components/Counter";
+import TenCounter from "./components/TenCounter";
+import ClassHandler from "./components/ClassHandler";
+import FunctionHandler from "./components/FunctionHandler";
 
-function App() {
-  return (
-    <div className="App">
-      {/* <Paul></Paul>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <Paul></Paul>
       <Welcome></Welcome> */}
-      <Next3></Next3>
-    </div>
-  );
+        {/* <Next3></Next3> */}
+        {/* <TenCounter /> */}
+        <ClassHandler />
+        <FunctionHandler />
+      </div>
+    );
+  }
 }
 
 export default App;
