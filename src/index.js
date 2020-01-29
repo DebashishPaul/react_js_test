d6b2e4eb0e7301b8685de0a96d976f79b3003762
Debashish Paul <debashish333paul@gmail.com>
2020-01-29 11:25:16 +0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,6 +4,41 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { createStore } from "redux";
+
+//action increment
+const increment = () => {
+  return {
+    type: "INCREMENT"
+  };
+};
+const decrement = () => {
+  return {
+    type: "DECREMENT"
+  };
+};
+
+//reducer
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 3;
+  }
+};
+
+//store->global state
+let store = createStore(counter);
+
+//display in the counsole
+
+store.subscribe(() => console.log(store.getState()));
+
+//dispatch
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(increment());
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
